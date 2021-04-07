d36a2bea7c7a3bf715a68b3adb101a7a462440d8
ok97 <omkhawshi0@gmail.com>
2021-04-07 22:33:40 +0530

    Added: Employee Present or Absent

---
+++ EmployeeWageComputationProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PracticeProblem
+{       /* Check Employee is 
+           Present or Absent
+           - Use ((RANDOM)) for Attendance Check
+        */
+    class EmployeeWageComputationProblem
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int EmpCheack = random.Next(0, 2);
+            if (EmpCheack == 1)
+            {
+                Console.WriteLine("Employee Is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employe Is Absent");
+            }
+
+        }
+    }
+}
