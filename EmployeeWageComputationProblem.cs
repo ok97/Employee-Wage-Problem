c98a113d8b59c91bd24459f0c41f0759a1ea804c
ok97 <omkhawshi0@gmail.com>
2021-04-16 11:59:39 +0530

    Added: save the Total Wage for Each Company

--- EmployeeWageComputationProblem.cs
+++ EmployeeWageComputationProblem.cs
@@ -4,28 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee_Wage_Computation_Problem
-{           /* UC8:- Ability to compute EmployeeWageformultiple companies */
-    class UC8EmployeeWageformultiplecompanies
+namespace PracticeProblem
+{       /* UC10 Ability to manage Employee Wage of multiple companies
+        */
+
+    class EmployeeWageComputationProblem
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalempwage;
+        public EmployeeWageComputationProblem(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int TotalEmpWage)
+        {
+            this.totalempwage = totalempwage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company :- " + company + " is :- " + totalempwage;
+        }
+    }
+    public class EmpWageBuilderArray
     {
         public const int FULL_TIME = 1;     //Constant variable
         public const int PART_TIME = 2;
-        // public const int EMP_RATE_PER_HOUR = 20;
-        //public const int MAX_WORKING_HRS = 100;
-        // public const int MAX_WORKING_DAYS = 20;
-        public static int ComputeEmployeeWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+
+        public int numofCompany = 0;
+        private EmployeeWageComputationProblem[] EmployeeWageComputationProblemArray;
+
+        public EmpWageBuilderArray()
+        {
+            this.EmployeeWageComputationProblemArray = new EmployeeWageComputationProblem[5];
+        }
+        public void addComapnyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            EmployeeWageComputationProblemArray[this.numofCompany] = new EmployeeWageComputationProblem(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numofCompany++;
+        }
+        public void ComputeEmployeeWage()
+        {
+            for (int i = 0; i < numofCompany; i++)
+            {
+                EmployeeWageComputationProblemArray[i].setTotalEmpWage(this.ComputeEmployeeWage(this.EmployeeWageComputationProblemArray[i]));
+                Console.WriteLine(this.EmployeeWageComputationProblemArray[i].toString());
+            }
+        }
+        private int ComputeEmployeeWage(EmployeeWageComputationProblem EmployeeWageComputationProblem)
         {
             int emphrs = 0;
             int empWage = 0;
             int totalempwage = 0;
             int hrs = 0;
             int workingDays = 1;
-            Random random = new Random();       //Random Class
-            while (hrs < empRatePerHour && numOfWorkingDays <= maxHoursPerMonth)
-            // for (int Day = 0; Day < NUM_OF_WORKING_DAYS; Day++)
+            while (emphrs <= EmployeeWageComputationProblem.maxHoursPerMonth && workingDays < EmployeeWageComputationProblem.numOfWorkingDays)
             {
 
                 workingDays++;
+                Random random = new Random();
                 int EmpCheack = random.Next(0, 3);      //Random Generate 0 ,1,2
                 switch (EmpCheack)          //Switch case Statment
                 {
@@ -39,27 +81,26 @@ namespace Employee_Wage_Computation_Problem
                         emphrs = 0;
                         break;
                 }
+                hrs += emphrs;
 
-                // Calculate empWage
-                hrs += emphrs;                                           //Display empwage
-                totalempwage = totalempwage + empWage;      //Calculate total employe month wage
-                empWage = empRatePerHour * emphrs;
+                Console.WriteLine("Day " + workingDays + " Emp hrs:- " + emphrs);//Display empwage
+
+                //totalempwage = totalempwage + empWage;      //Calculate total employe month wage
+                //empWage = empRatePerHour * emphrs;
+
             }
-
-            Console.WriteLine($"Toatal Emp wage for company:- {company} is {totalempwage}");
-            Console.WriteLine("Employe Wage Per Day :- " + empWage);
-            //Console.WriteLine("Total Employe Month Wage :- " + totalempwage);
-            //  Console.WriteLine("Employee wage for " + workingDays + " days " + totalempwage);
-            Console.WriteLine("Working hours " + hrs + "\n");
-            return totalempwage;
-
+            return emphrs * EmployeeWageComputationProblem.empRatePerHour;
         }
-        static void Main(String[] args)
+
+    }
+    class Program
+    {
+        public static void Main(String[] args)
         {
-            ComputeEmployeeWage("Dmart", 20, 2, 10);
-            ComputeEmployeeWage("Walmart", 20, 20, 50);
-            ComputeEmployeeWage("Freshmart ", 10, 6, 40);
-            ComputeEmployeeWage("Budget Foods", 20, 4, 10);
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();   //Create object of class
+            empWageBuilder.addComapnyEmpWage("Dmart", 20, 2, 10);  //call method
+            empWageBuilder.addComapnyEmpWage("Relicance", 10, 4, 20); //call method
+            empWageBuilder.ComputeEmployeeWage(); //call method
         }
     }
 }
